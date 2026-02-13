b504f4e5f02c3dea9c71c028b3cc5ee5bf3a9bf7
krishnanbala123 <krishnanbala7183@gmail.com>
2026-02-13 11:25:09 +0530

    image tag

--- src/components/AdminSidebar.js
+++ src/components/AdminSidebar.js
@@ -24,7 +24,7 @@ export default function AdminSidebar({
       {/* 🔥 BRAND LOGO */}
       <div className="sidebar-brand">
         <img
-          src="/bricks-logo.png"
+          src="/brickslogo.png"
           alt="RS Bricks"
           width={collapsed ? 36 : 120}
           height={80}
